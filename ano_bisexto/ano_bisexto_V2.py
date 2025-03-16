9174b02d7efc9668b1b65f4beb615d5b7848c64e
Rafael Marinho <fhaelmarinho@gmail.com>
2025-03-15 21:59:51 -0300

    feat:Versão refatorada do código

---
+++ ano_bisexto/ano_bisexto_V2.py
@@ -0,0 +1,16 @@
+def verificador_ano_bissexto():
+    while True:
+    # Solicita ao usuário que informe o ano
+        ano = int(input("Informe o ano: "))
+
+    # Verifica se o ano é bissexto
+        if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
+            print("SIM")
+        if ano == 0:
+            print("Encerrando...")
+            break    
+        else:
+            print("NÃO")
+
+# Chama a função
+verificador_ano_bissexto()
